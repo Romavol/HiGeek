29f474d0cec99888f4965856c101d16017ac4ff0
Roman Volkovich <romavol@gmail.com>
2019-12-20 22:17:52 +0300

    4ре задачи

--- 1Hello.py
+++ 1Hello.py
@@ -1,12 +1,49 @@
+###
 
 
-var_num = 12 + 4
-var_str = "Some string ."
-print(" var xx = " + str(var_num))
-print(var_str)
+#
+
+#var_num = 12 + 4
+#var_str = "Some string ."
+
+#print("1. var num = " , str(var_num), ' and summ 220 + 45 is ' , 220+45)
+#print("1. " + var_str)
+
+
+#user = input(" Введи свой никНейм : ")
+#week = int(input(" Введи сегодняшний день  недели числом : "))
+
+#if week < 0:
+#    print(" Чтото пошло не так с числом...")
+#elif week == 1: print(user,", сегодня Пнд.")
+#elif week == 2: print(user, " , сегодня Втр.")
+#elif week == 3: print(user, " , сегодня Срд.")
+#elif week == 4: print(user, " , сегодня Четв.")
+#elif week == 5: print(user, " , сегодня Пятн.")
+#else: print(user, " , вот и Выходной.")
+
+# часть 2
+'''
+secu = int(input(" 2. Введи мноого секунд. Например с утра : "))
+hours = secu//3600
+
+minutes = (secu - (3600 * hours))//60
+secunds = (secu - (3600 * hours + minutes * 60))
+
+print( hours, minutes, secunds, sep=':')
+
+
+# Часть 3
+
+snum1 = input("3. Введи число n до 10 :")
+snum2 = snum1+snum1
+snum3 = snum2+snum1
 
 
 
+print("Сумма 3х чисел ( n + nn + nnn ) равна = ", int(snum1)+int(snum2)+int(snum3))
+'''
 
+snum = input("4. Введи целое положительное многозначное число  :")
 
 
