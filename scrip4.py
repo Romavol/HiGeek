b5c06a7bf364a16854f1f7a5af1e08e6380968df
Roman Volkovich <romavol@gmail.com>
2020-01-17 02:05:39 +0300

    два файла - скриптовый и функцианальный

--- scrip4.py
+++ scrip4.py
@@ -1,1 +1,11 @@
 # Задания 4 урок
+
+from sys import argv
+
+script_name, first_param, second_param = argv
+
+hour = float(first_param)
+prem = float(second_param)
+tariff = 15  # зададим 1 вручную
+
+print("Cумма зарплаты : ", (hour * 15 + prem))
